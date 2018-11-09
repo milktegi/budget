7b791d4e854c606984c28af3cf0dfcb3da0af146
mikltegi <milkteegi@gmail.com>
2018-11-09 15:19:28 +0900

    added eventListener

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 // 즉시 호출 함수 iffy 
 // Budget Controller
-var budgetController = (function(){
+var budgetController = (function () {
 
-	
+
 
 
 })();
 
 // UI Controller
-var UIController = (function(){
+var UIController = (function () {
 
-// central strings 
-var DOMstrings = {
-	inputType: '.add__type',
-	inputDesc: '.add__description',
-	inputVal: '.add__value',
-	inputButton: '.add_btn'
-}
+	// central strings 
+	var DOMstrings = {
+		inputType: '.add__type',
+		inputDesc: '.add__description',
+		inputVal: '.add__value',
+		inputButton: '.add_btn'
+	}
 
 
-// return an object
+	// return an object
 	return {
-		getInput: function() {
+		getInput: function () {
 
-		return {	
-			// select first 
-		type: document.querySelector(DOMstrings.inputType).value,
-		// and then do sth with it in this case will be either inc or exp
-		description: document.querySelector(DOMstrings.inputDesc).value,
-		value: document.querySelector(DOMstrings.inputVal).value	
-			// to return three at the same time => obj 
+			return {
+				// select first 
+				type: document.querySelector(DOMstrings.inputType).value,
+				// and then do sth with it in this case will be either inc or exp
+				description: document.querySelector(DOMstrings.inputDesc).value,
+				value: document.querySelector(DOMstrings.inputVal).value
+				// to return three at the same time => obj 
 
 			}
 		},
 
-		getDOMstrings: function() {
+		getDOMstrings: function () {
 			return DOMstrings;
 		}
 	}
@@ -42,52 +42,51 @@ var DOMstrings = {
 })();
 
 // Global App Controller
-var controller = (function(budgeCtrl, UICtrl){
-  
-// model can receive arguments
+var controller = (function (budgetCtrl, UICtrl) {
 
-var DOM = UICtrl.getDOMstrings();
+	// model can receive arguments
 
-var ctrlAddItem = function() {
+	var setupEventListeners = function () {
 
-// console.log('button was clicked!');
-	// 1. get the field Input data
-var input = UICtrl.getInput();
-console.log(input);
-	// not just from clicking but also pressing the enter key 
+	var DOM = UICtrl.getDOMstrings();
+		document.querySelector(DOM.inputButton)
+				.addEventListener('click', ctrlAddItem); 
+		document.addEventListener('keypress', function (event) {
+			if (event.keycode == 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+			// get data, take what ui iffy returns 
+		});
+	};
 
-	// 2. add the Item to the budget controller
+	var ctrlAddItem = function () {
 
-	// 3. add the Item to user Interfact as well
+		// console.log('button was clicked!');
+		// 1. get the field Input data
+		var input = UICtrl.getInput();
+		console.log(input);
+		// not just from clicking but also pressing the enter key 
 
-	// 4. in budgetController calculate budget =>
+		// 2. add the Item to the budget controller
 
-	// 5. in UI display that data 
-	// console.log('it works!');
-}
+		// 3. add the Item to user Interfact as well
 
+		// 4. in budgetController calculate budget =>
 
-document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+		// 5. in UI display that data 
+		// console.log('it works!');
+	};
 
-// handle kep pressing event as well 
-// because keyEvent happens anywhere in document 
-document.addEventListener('keypress', function(event){
-	// exicute 
-	// console.log(event); // -> keycode is 13(enter pressed)
-	// for older browers added .which as well 
-	if(event.keycode == 13 || event.which === 13) {
-		ctrlAddItem();
+	return {
+		init: function() {
+			console.log('App has started!');
+			setupEventListeners();
+		}
 	}
-	// get data, take what ui iffy returns 
-
-
-
-
-});
-
-
 
 })(budgetController, UIController);
 
+// call init function outside 
 
+controller.init();
 
