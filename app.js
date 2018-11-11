ea33dec30a2dd34004323d9cf28ea449911352f9
mikltegi <milkteegi@gmail.com>
2018-11-11 15:22:49 +0900

    improved date func and ui

--- app.js
+++ app.js
@@ -156,9 +156,25 @@ var UIController = (function () {
 		expensesLabel: '.budget__expenses--value',
 		percentageLabel: '.budget__expenses--percentage',
 		container: '.container',
-		expensesPercLabel: '.item__percentage'
+		expensesPercLabel: '.item__percentage',
+		dateLabel: '.budget__title--month'
 	}
 
+
+	var formatNumber = (num, type) => {
+
+			num = Math.abs(num);
+			num = num.toFixed(2); 
+
+			numSplit = num.split('.');
+
+			int = numSplit[0];
+			if(int.length > 3) {
+				int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3); // input 2310, output 2,310
+			}
+
+				return (type === 'exp' ? sign ='-' : sign= '+') + ' ' + int;
+		};
 
 	// return an object
 	return {
@@ -209,7 +225,7 @@ var UIController = (function () {
 
 			newHtml = html.replace('%id%', obj.id);
 			newHtml = newHtml.replace('%description%', obj.description);
-			newHtml = newHtml.replace('%value%', obj.value);
+			newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
 
 			// insert it into the dom => jsonhtmlmethod
 			document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
@@ -238,9 +254,12 @@ var UIController = (function () {
 
 		displayBudget: function (obj) {
 
-			document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
-			document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
-			document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+			var type;
+			obj.budget > 0 ? type = 'inc' : type = 'exp';
+
+			document.querySelector(DOMstrings.budgetLabel).textContent = formatNumber(obj.budget);
+			document.querySelector(DOMstrings.incomeLabel).textContent = formatNumber(obj.totalInc, 'inc');
+			document.querySelector(DOMstrings.expensesLabel).textContent = formatNumber(obj.totalExp, 'exp');
 
 			if (obj.percentage > 0) {
 				document.querySelector(DOMstrings.percentageLabel).textContent 
@@ -274,6 +293,26 @@ var UIController = (function () {
 			});
 
 		}, 
+
+		// Date object 
+
+		displayMonth: function() {
+
+			var now, year, month, months;
+			now = new Date();
+			months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
+			month = now.getMonth();
+			// var christmast = new Date(2018, 11, 25);
+			year = now.getFullYear();
+			console.log(year);
+			document.querySelector(DOMstrings.dateLabel).textContent = year + '년 ' + months[month] + '월';
+
+		},
+
+		// + or - before the number 
+		// comma seperating manipulate Strings 1,000,000
+	
+
 
 		getDOMstrings: function () {
 			return DOMstrings;
@@ -377,6 +416,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 	return {
 		init: function () {
 			console.log('App has started!');
+			UICtrl.displayMonth(); 
 			UICtrl.displayBudget({
 				budget: 0,
 				totalInc: 0,
@@ -384,6 +424,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 				percentage: -1
 			});
 			setupEventListeners();
+			
 
 		}
 	}
